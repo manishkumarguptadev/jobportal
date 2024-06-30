7f9c4d85a3a643a4e3d0117da59316a1f61402cf
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-06-30 18:39:55 +0530

    usejobitems custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import JobDetails from "./components/JobDetails";
 import Sidebar from "./components/Sidebar";
-import JobItem from "./types/JobItem";
-import { BASE_API_URL } from "@/lib/constants";
-import axios, { AxiosError, CanceledError } from "axios";
+import { useState } from "react";
+import useJobItems from "./hooks/useJobItems";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const [jobItems, setJobItems] = useState<JobItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const controller = new AbortController();
-    if (!searchText) return;
-    const fetchJobItems = async () => {
-      try {
-        setIsLoading(true);
-        const res = await axios.get<JobItem[]>(
-          `${BASE_API_URL}?search=${searchText}`,
-          { signal: controller.signal },
-        );
-        setIsLoading(false);
-        setJobItems(res.data);
-      } catch (error) {
-        if (error instanceof CanceledError) return;
-        setIsLoading(false);
-        if (error instanceof AxiosError) {
-          const message = error.response?.data.message || "Server Unavailable";
-          setError(message);
-        }
-      }
-    };
-
-    fetchJobItems();
-    return () => controller.abort();
-  }, [searchText]);
+  const [jobItems, isLoading, error] = useJobItems(searchText);
   return (
     <div>
       <Header searchText={searchText} setSearchText={setSearchText} />
