0f9158f0e89950cd81972a65d468fa40864a0d6f
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-07-20 19:12:43 +0530

    active joblist item capture

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Header from "./components/Header";
 import JobDetails from "./components/JobDetails";
 import Sidebar from "./components/Sidebar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useJobItems from "./hooks/useJobItems";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const [jobItems, isLoading, error] = useJobItems(searchText);
+  const [activeId, setActiveId] = useState<string | null>(null);
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
   return (
     <div>
       <Header searchText={searchText} setSearchText={setSearchText} />
